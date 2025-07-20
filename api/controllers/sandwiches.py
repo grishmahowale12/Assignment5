113914cf12218b68ee508224ff7e7dd9b412ca5a
grishmahowale <grishmahowale@gmail.com>
2025-07-19 23:54:42 -0400

    commit changes into the main.py

---
+++ api/controllers/sandwiches.py
@@ -0,0 +1,34 @@
+from sqlalchemy.orm import Session
+from fastapi import HTTPException, status, Response
+from ..models import models
+from ..models import schemas
+
+def create(db: Session, sandwich):
+    db_sandwich = models.Sandwich(
+        name=sandwich.name,
+        price=sandwich.price,
+        description=sandwich.description
+    )
+    db.add(db_sandwich)
+    db.commit()
+    db.refresh(db_sandwich)
+    return db_sandwich
+
+def read_all(db: Session):
+    return db.query(models.Sandwich).all()
+
+def read_one(db: Session, sandwich_id):
+    return db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id).first()
+
+def update(db: Session, sandwich_id, sandwich):
+    db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
+    update_data = sandwich.dict(exclude_unset=True)
+    db_sandwich.update(update_data, synchronize_session=False)
+    db.commit()
+    return db_sandwich.first()
+
+def delete(db: Session, sandwich_id):
+    db_sandwich = db.query(models.Sandwich).filter(models.Sandwich.id == sandwich_id)
+    db_sandwich.delete(synchronize_session=False)
+    db.commit()
+    return Response(status_code=status.HTTP_204_NO_CONTENT)
